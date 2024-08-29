babf96d7c4d3af790981205f30097b437c2a2bd3
JH <jhk3261@gnu.ac.kr>
2024-08-29 13:24:06 +0900

    뽀모도로앱 #1
    - UI디자인 완료
    - 한 화면 만들기
    - flexible 공부(flex 속성으로 비율 조정)
    - 구역 너비 확장하는 법

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-//UI구축
-class _AppState extends State<App> {
-  // List<int> numbers = [];
-  // int counter = 0;
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
-  void onClicked() {
-    setState(() {
-      //데이터변경 알리는 함수 (없다면 build함수 실행 안 됨)
-      //데이터 변경을 무조건 여기 넣어야하나? 답은 노. (넣으면 가독성은 좋다.)
-      // numbers.add(numbers.length);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.red,
-            ),
+      theme: ThemeData(
+        //backgroundColor 바로 쓰면 오류! (옛날버젼)
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
+        textTheme: const TextTheme(
+          //headline1 오류
+          headlineLarge: TextStyle(
+            color: Color(0XFF232B55),
           ),
         ),
-        home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                showTitle ? const MyLargeTitle() : const Text('nothing'),
-                IconButton(
-                    onPressed: toggleTitle,
-                    icon: const Icon(Icons.remove_red_eye))
-                // for (var n in numbers) Text('$n'),
-                // Text(
-                //   '$counter',
-                //   style: const TextStyle(
-                //     fontSize: 20,
-                //   ),
-                // ),
-                // IconButton(
-                //     onPressed: onClicked, icon: const Icon(Icons.add_box_rounded))
-              ],
-            ),
-          ),
-        ));
-  }
-}
-
-//appstate에서 위젯 분리
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    //상태초기화메소드 -> 부모요소에 의존하는 데이터 초기화 시 사용
-    //항상 build보다 먼저 호출 (단 한 번만 호출됨)
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    //api업데이트, 이벤트 리스너로부터 구독 취소나 form의 리스너에서 벗어날 때,
-    //무언가를 취소하는 곳.
-    super.dispose();
-  }
-
-  //context : 상위요소의 모든 정보 (부모요소에 대한 모든 정보)
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.bold,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-        //!(느낌표) or ?(물음표)
+        cardColor: const Color(0XFFF4EDD8),
       ),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        children: [
+          //UI기반 유연하게 만들어줌
+          Flexible(
+            flex: 1, //기본값 (비율을 설정할 수 있다)
+            child: Container(
+              alignment: Alignment.bottomCenter, //찾아보기?
+              child: Text(
+                "25:00",
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
+                onPressed: () {},
+                icon: const Icon(Icons.play_circle_outline),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              //밑구역 너비 확장 1번
+              children: [
+                Expanded(
+                  //밑구역 너비 확장 2번
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).cardColor),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodors',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            fontSize: 58,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
